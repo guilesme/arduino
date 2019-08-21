cb9ba0e228a07fb1fc66c8db5a91b8f685e72a5b
guilesme <48301490+guilesme@users.noreply.github.com>
2019-08-20 22:11:23 -0300

    Add files via upload

---
+++ Aula_1/hello_world/hello_world.ino
@@ -0,0 +1,14 @@
+/*Guilherme
+ * https://subscription.packtpub.com/book/hardware_and_creative/9781785289088/1/ch01lvl1sec11/hello-world
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello World!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
