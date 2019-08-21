575f721cc02717d5eef0cf97ca6c6e9b41d01575
guilesme <48301490+guilesme@users.noreply.github.com>
2019-08-20 22:15:08 -0300

    Add files via upload

---
+++ SuperMaquina/supermaquina.ino
@@ -0,0 +1,14 @@
+/**
+   Sequencial de leds(supermaquina effect)
+   @author - Guilherme
+*/
+void setup() {
+  //define o pino 13 como saida
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  //mandar 5v no pino 13 
+  digitalWrite(13, HIGH);
+  //digitalWrite(13, LOW);
+}
